f3ffa56ad772a9d24084021bed095ce00aee6a40
Manfredo San Bonifacio <manfredo@disanbonifacio.com>
2026-02-14 10:45:39 +0100

    Initial Commit

---
+++ QuizLicenzaCacciaToscana/QuizLicenzaCacciaToscanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizLicenzaCacciaToscanaApp.swift
+//  QuizLicenzaCacciaToscana
+//
+//  Created by Manfredo San Bonifacio on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct QuizLicenzaCacciaToscanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
